6754a955bf8fadbdd83786fd7b3542ef50bfab07
Roman <roman@MacBook-Air-Roman.local>
2022-08-22 09:15:36 +0900

    Example009_IntroArray added

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,51 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+//             0  1  2  3  4  5  6  7  8
+int[] array = {17,7,15,24,45,51,62,211,87};
+//array[0] = 12;
+//Console.WriteLine(array[0]);
+
+
+// int max1 = Max(a1, b1, c1);
+
+// int max2 = Max(a2, b2, c2);
+
+// int max3 = Max(a3, b3, c3);
+
+
+
+/*
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+*/
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(result);
+
+
+
+
+
+
+
+
+
