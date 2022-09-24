87fb973ff6ecfb9dc2a699b24e6ff73078891148
roman <tsoyroman29@gmail.com>
2022-09-25 00:33:50 +0900

    Homework

---
+++ ConsoleTriangle/ConsoleTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using Triangle;
+namespace ConsoleTriangle
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            PascalClass PascalTriangle = new PascalClass();
+            PascalTriangle.row = 9;
+            PascalTriangle.triangle = new int[9, 9];
+
+            PascalTriangle.FillTriangle();
+            Console.ReadLine();
+            PascalTriangle.Magic();
+            Console.ReadLine();
+
+
+
+        }
+
+    }
+
+}
+
